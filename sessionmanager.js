9baf242df7b1bedaaf4a283f22c45ab6a0b0ea59
kimt1 <forbidden.tech@gmail.com>
2025-07-06 18:23:32 +0800

    Add sessionmanager.js

---
+++ sessionmanager.js
@@ -0,0 +1,4 @@
+throw new Error(
+  "sessionmanager.js is deprecated. Use 'session-manager.js' as the canonical session manager. " +
+  "Remove any dependencies on 'sessionmanager.js' and update all imports/requires to use 'session-manager.js'."
+);
