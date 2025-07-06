3faa4f0af2c1b31a62f62ccb935fe60ee0bcb306
kimt1 <forbidden.tech@gmail.com>
2025-07-06 18:23:34 +0800

    Add fallbackstrategies.js

---
+++ fallbackstrategies.js
@@ -0,0 +1,8 @@
+/* 
+    Deprecated: fallbackstrategies.js
+    See 'fallback-strategies.js' for all fallback logic.
+*/
+
+throw new Error(
+    "[deprecated] fallbackstrategies.js is obsolete. Use 'fallback-strategies.js' instead."
+);
